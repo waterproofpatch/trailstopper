59ca729c44e2c5e9bcf3aa93c6a92eadc6625d5c
waterproofpatch <waterproofpatch@gmail.com>
2021-10-10 00:35:15 -0400

    delete button

--- app/src/main/java/com/example/trailstopper/StockAdapter.java
+++ app/src/main/java/com/example/trailstopper/StockAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,7 +30,7 @@ public class StockAdapter extends RecyclerView.Adapter<StockAdapter.Viewholder> 
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StockAdapter.Viewholder holder, int position) {
+    public void onBindViewHolder(@NonNull final StockAdapter.Viewholder holder, int position) {
         // to set data to textview and imageview of each card layout
         Stock stock = stockArrayList.get(position);
         if (stock == null) {
@@ -40,6 +41,14 @@ public class StockAdapter extends RecyclerView.Adapter<StockAdapter.Viewholder> 
         holder.price.setText("Current: " + stock.getPrice());
         holder.atr.setText("ATR: " + stock.getAtr());
         holder.trailStop.setText("Trail Stop: " + stock.getTrailStop() + " (" + stock.getTrailStopPct() +"%)");
+
+        holder.buttonRemoveStock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stockArrayList.remove(holder.getAdapterPosition());
+                notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
@@ -57,14 +66,17 @@ public class StockAdapter extends RecyclerView.Adapter<StockAdapter.Viewholder> 
         private TextView atr;
         private TextView name;
         private TextView trailStop;
+        private Button buttonRemoveStock;
 
-        public Viewholder(@NonNull View itemView) {
+        public Viewholder(@NonNull final View itemView) {
             super(itemView);
             ticker = itemView.findViewById(R.id.ticker);
             name = itemView.findViewById(R.id.name);
             price = itemView.findViewById(R.id.price);
             atr = itemView.findViewById(R.id.atr);
             trailStop = itemView.findViewById(R.id.trail_stop);
+            buttonRemoveStock = (Button)itemView.findViewById(R.id.buttonRemoveStock);
+
         }
     }
 }
